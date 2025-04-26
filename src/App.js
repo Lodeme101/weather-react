bc7ec4992d967931406300addac52ead1c5dc729
Lodeme101 <lodeme101@gmail.com>
2025-04-26 11:40:17 +0900

    Added GitHub link

--- src/App.js
+++ src/App.js
@@ -2,5 +2,14 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        {" "}
+        This project was coded by Lodemé Pendergraph and open-sourced on{" "}
+        <a href="https://github.com/Lodeme101/weather-react">GitHub</a>
+      </footer>
+    </div>
+  );
 }
