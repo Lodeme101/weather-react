3032b3abff922c3f9be17b48add4e0df439eaf78
Lodeme101 <lodeme101@gmail.com>
2025-04-27 10:33:23 +0900

    Added js files, installed npm packets

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
 import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        {" "}
-        This project was coded by Lodemé Pendergraph and open-sourced on{" "}
-        <a href="https://github.com/Lodeme101/weather-react">GitHub</a>
-      </footer>
+      <div className="container">
+        <Weather defaultCity="Pretoria" />
+
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/lodeme-pendergraph/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Lodemé Pendergraph
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/Lodeme101/weather-react"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://weatherinreact.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
